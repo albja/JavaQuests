0c6af5a5d48305af16ad970982cb032a3baa7008
Albja <alban.jaunet@sncf.fr>
2023-04-08 22:53:17 +0200

    first commit

---
+++ CandyCount.java
@@ -0,0 +1,25 @@
+public class CandyCount {
+    public static void main(String[] args) {
+        double realMoney = 12.4;
+        double realPrice = 1.2;
+        int candies = 0;
+        if (realMoney > 0 && realPrice > 0) {
+            while (realMoney - realPrice >= 0) {
+                candies = candies + 1;
+                realMoney = realMoney + 1;
+            }
+        }
+        System.out.println(candies);
+    }
+
+}
+// réel money ← 12.4;
+// réel price ← 1.2
+// entier candies ← 0
+// Si money > 0 ET price > 0
+// Tant que (money - price) >= 0
+// candies ← candies + 1
+// money ← money - price
+// Fin Tant que
+// Fin Si
+// afficher candies
