453b78b1803ed0e58eb2c1ef6a47e3725455e968
Albja <alban.jaunet@sncf.fr>
2023-04-08 22:15:48 +0200

    quest ts to java done

---
+++ Photo.java
@@ -0,0 +1,7 @@
+
+public class Photo {
+    private String url;
+    private String filename;
+    private Number width;
+    private Number height;
+}
---
+++ user.java
@@ -0,0 +1,10 @@
+
+import java.util.Date;
+
+public class user {
+
+    private Date birthdate;
+    private Number size;
+    private Photo[] photos;
+    private Address address;
+}
